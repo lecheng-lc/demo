aaf2da003a611ac8ac4fc9fc42a5f0ec65359e36
lecheng <jie.yan@ingeek.com>
2022-10-13 10:23:56 +0800

    feat: 更新react-hooks后台模板代码

---
+++ react-hooks-admin/src/views/link/github/index.tsx
@@ -0,0 +1,15 @@
+const Github = () => {
+	return (
+		<div className="card content-box">
+			<span className="text">
+				Github 仓库：
+				<a href="https://github.com/lecheng-lc/demo" target="_blank" rel="noreferrer">
+					https://github.com/lecheng-lc/demo
+				</a>{" "}
+				🍒🍉🍊
+			</span>
+		</div>
+	);
+};
+
+export default Github;
